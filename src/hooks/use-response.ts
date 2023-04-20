baf4afbba8226c33336679b72e9269ae16288ae4
Ali <ali.hussein.pre@gmail.com>
2023-04-20 02:37:46 +0200

    feat: use streaming

--- src/hooks/use-response.ts
+++ src/hooks/use-response.ts
@@ -1,10 +1,14 @@
 import { create } from "zustand";
-
+type Status = "loading" | "success" | "error" | "idle";
 interface State {
   response?: string;
   setResponse: (response: string) => void;
+  status?: Status;
+  setStatus: (status: Status) => void;
 }
 
 export const useResponse = create<State>((set) => ({
   setResponse: (response) => set({ response }),
+  status: "idle",
+  setStatus: (status) => set({ status }),
 }));
