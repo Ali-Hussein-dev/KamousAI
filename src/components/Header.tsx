b89aa7721940bb122eaf93e237d0a7e53e2fcfac
Ali <ali.hussein.pre@gmail.com>
2023-04-25 17:01:58 +0200

    feat: auth client-side logic & UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,47 @@
-import { Anchor, Header as MantineHeader } from "@mantine/core";
+import {
+  ActionIcon,
+  Anchor,
+  Avatar,
+  Button,
+  Header as MantineHeader,
+  Tooltip,
+} from "@mantine/core";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 
 //======================================
+export const UserDropdown = () => {
+  const { data: sessionData } = useSession();
+  // const [opened, { toggle }] = useDisclosure(false);
+  return (
+    <ActionIcon onClick={() => void signOut()}>
+      <Tooltip label="Logout">
+        <Avatar src={sessionData?.user?.image as string} radius="xl">
+          {sessionData?.user?.name}
+        </Avatar>
+      </Tooltip>
+    </ActionIcon>
+  );
+};
+//======================================
 export const Header = () => {
+  const { data: sessionData } = useSession();
+
   return (
     <MantineHeader height={60} p="xs" className="flex-row-center">
-      <div className="mx-auto w-full max-w-4xl flex-row-start">
-        <Anchor color="gray" href="/" target="_blank">
+      <div className="mx-auto w-full max-w-4xl flex-row-between ">
+        <Anchor color="gray" href="/">
           <div className="text-xl font-bold">KamousAI</div>
         </Anchor>
+        {sessionData ? (
+          <UserDropdown />
+        ) : (
+          <Link href="/signin">
+            <Button variant="outline" color="gray" radius="md">
+              Login
+            </Button>
+          </Link>
+        )}
       </div>
     </MantineHeader>
   );
---
+++ src/pages/signin.tsx
@@ -0,0 +1,70 @@
+import { Button, Title, Paper, Divider } from "@mantine/core";
+import { type GetServerSidePropsContext } from "next";
+import { getProviders, getSession, signIn } from "next-auth/react";
+
+import * as React from "react";
+import { BsGithub, BsGoogle } from "react-icons/bs";
+
+//======================================
+const Signin = ({
+  providers,
+}: {
+  providers: Awaited<ReturnType<typeof getProviders>>;
+}) => {
+  return (
+    <div className="grid min-h-screen place-items-center px-4">
+      <Paper
+        radius="lg"
+        shadow="lg"
+        p="lg"
+        pt={0}
+        withBorder
+        className="max-w-lg px-4 "
+        w="100%"
+      >
+        <div className="w-full">
+          <Title order={2} ta="center">
+            Log in
+          </Title>
+          <Divider w="100%" />
+        </div>
+        <div className="gap-y-4 pt-8 flex-col-center">
+          {providers &&
+            Object.values(providers).map((prov) => (
+              <Button
+                key={prov.name}
+                variant="outline"
+                color="gray"
+                size="lg"
+                radius="md"
+                leftIcon={prov.id === "github" ? <BsGithub /> : <BsGoogle />}
+                onClick={() => void signIn(prov.id)}
+              >
+                {prov.name}
+              </Button>
+            ))}
+        </div>
+      </Paper>
+    </div>
+  );
+};
+
+export default Signin;
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  const providers = await getProviders();
+  const session = await getSession(ctx);
+
+  if (session)
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  return {
+    props: {
+      providers,
+    },
+  };
+}
