295c63f1bc6006400681387c6bb6838023fbc069
Ali <ali.hussein.pre@gmail.com>
2023-05-14 16:25:26 +0200

    fix: defer loading of Client Components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { type NextPage } from "next";
 import Head from "next/head";
-import { AppShell } from "@mantine/core";
-import { Footer, Header, Searchbar, Response } from "@/components";
+import { AppShell, Skeleton } from "@mantine/core";
+import { Footer, Header } from "@/components";
+import dynamic from "next/dynamic";
+
+const Searchbar = dynamic(() => import("../components/Search-Bar").then(c => c.Searchbar), {
+  ssr: false,
+  loading: () => <Skeleton h={48} radius="lg" my="md" w="100%" opacity="0.3" />
+})
+const Response = dynamic(() => import("../components/Response").then(c => c.Response), {
+  ssr: false,
+  loading: () => <Skeleton h={248} radius="lg" my="md" w="100%" opacity="0.3" />
+})
 
 const Home: NextPage = () => {
   return (
