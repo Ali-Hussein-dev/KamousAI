38959d04ef12dd94190073c1d85aa708c8ea30ed
Ali <ali.hussein.pre@gmail.com>
2023-08-06 20:03:24 +0200

    chore: install mantine v7 alpha release

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import { type Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       typography: (_theme: unknown) => ({
