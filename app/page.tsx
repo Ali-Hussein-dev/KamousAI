d959a1c124f36757f65811f38c9cfa5203f20c0e
Ali <ali.hussein.pre@gmail.com>
2023-09-27 11:54:55 +0200

    feat: tweak the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,39 @@
 "use client";
 import { languagetoolsList } from "@/components";
-import { Card, Title } from "@mantine/core";
+import { Badge, Button, Card, Text, Title } from "@mantine/core";
 import { type NextPage } from "next";
 import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
-    <div className="mx-auto h-full w-full max-w-2xl pt-5 sm:pt-14">
-      <div className="mx-auto max-w-xl space-y-3">
-        <Title order={2} ta="center">
+    <div className="mx-auto h-full w-full max-w-3xl pt-5 sm:pt-16 pb-3">
+      <div className="mx-auto space-y-3">
+        <Title order={2} ta="center" mb="lg">
           Language Tools
         </Title>
-        {languagetoolsList.map((item) => (
-          <Card key={item.label}>
-            <Link
-              href={item.href}
-              className="font-semibold text-slate-100 no-underline"
-            >
-              {item.label}
-            </Link>
-          </Card>
+        <div className="grid sm:grid-cols-2 grid-cols-1 gap-4 w-full">
+        {languagetoolsList.map(({ status, label, href, description }) => (
+          <div key={label} className="bg-gradient-to-b from-slate-600 to-transparent p-4 rounded">
+            <div className="mb-2 gap-2 flex-row-between">
+              <Title order={3}>{label}</Title>
+              {status !== "done" && <Badge>{status}</Badge>}
+            </div>
+            <Text c="gray">{description}</Text>
+            <div className="mr-2 flex-row-end">
+              {status === "done" && (
+                <Link
+                  href={href}
+                  className="font-semibold text-slate-100 no-underline"
+                >
+                  <Button variant="outline" py="sm">
+                    Use
+                  </Button>
+                </Link>
+              )}
+            </div>
+          </div>
         ))}
+        </div>
       </div>
     </div>
   );
--- src/components/tools-menu.tsx
+++ src/components/tools-menu.tsx
@@ -4,8 +4,30 @@ import Link from "next/link";
 import { TbChevronDown } from "react-icons/tb";
 
 export const languagetoolsList = [
-  { label: "Dictionary", href: "/tools/dictionary" },
-  { label: "Grammer Correction", href: "/tools/grammer-correction" },
+  {
+    label: "Dictionary",
+    href: "/tools/dictionary",
+    status: "done",
+description: "A tool for looking up the meanings and definitions of words.",
+  },
+  {
+    label: "Grammer Corrector",
+    href: "/tools/grammer-correction",
+    status: "done",
+    description: "An application that helps correct grammar and punctuation errors in text.",
+  },
+  {
+    label: "Reverse Dictionary",
+    href: "/tools/reverse dictionary",
+    status: "in progress",
+    description: "A feature that allows you to find words based on their meanings or descriptions.",
+  },
+  {
+    label: "Text Optimizer",
+    href: "/tools/reverse dictionary",
+    status: "planned",
+    description: "An upcoming tool designed to optimize and improve written text for clarity and style.",
+  },
 ];
 //======================================
 export const ToolsMenu = () => {
@@ -17,15 +39,17 @@ export const ToolsMenu = () => {
         </Button>
       </Menu.Target>
       <Menu.Dropdown p="sm">
-        {languagetoolsList.map((item) => (
-          <Link
-            key={item.label}
-            href={item.href}
-            className="w-full no-underline"
-          >
-            <Menu.Item key={item.label}>{item.label}</Menu.Item>
-          </Link>
-        ))}
+        {languagetoolsList
+          .filter((o) => o.status === "done")
+          .map((item) => (
+            <Link
+              key={item.label}
+              href={item.href}
+              className="w-full no-underline"
+            >
+              <Menu.Item key={item.label}>{item.label}</Menu.Item>
+            </Link>
+          ))}
       </Menu.Dropdown>
     </Menu>
   );
