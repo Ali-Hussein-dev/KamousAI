706ec3f7a0c273cd2108b56d7b7118d56a2c53c1
Ali <ali.hussein.pre@gmail.com>
2023-10-19 11:08:14 +0200

    feat: update footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,16 +8,16 @@ import { FaTwitter } from "react-icons/fa";
 export const Footer = () => {
   
   return (
-    <AppShell.Footer
-      p="xs"
-      className="!relative"
-      withBorder={false}
-    >
-      {/* <div className="h-[0.5px] bg-gradient-to-r from-transparent via-white to-transparent max-w-4xl mx-auto mb-1" /> */}
-      <div className="h-[0.5px] bg-white/40 max-w-4xl mx-auto mb-2" />
+    <AppShell.Footer p="xs" className="!relative" withBorder={false}>
+      {/* <div className="mx-auto mb-1 h-[0.5px] max-w-4xl bg-gradient-to-r from-transparent via-white to-transparent" /> */}
+      <div className="mx-auto mb-2 h-[0.5px] max-w-4xl bg-white/40" />
       <div className="mx-auto w-full max-w-4xl flex-row-between">
         <span>
-          Powered by <b>AI</b>
+          {/* ©{new Date().getFullYear()} */}
+          Made by{" "}
+          <a href="https://ali-hussein.com" target="_blank">
+            AH
+          </a>
         </span>
         <div className="gap-3 flex-row-start">
           <Anchor
