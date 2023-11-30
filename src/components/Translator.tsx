7cf3079ade41785ec7b99f6c0dc45f49c8ad0ed9
Ali <ali.hussein.pre@gmail.com>
2023-11-30 15:30:57 +0100

    feat: add loading skeleton

--- src/components/Translator.tsx
+++ src/components/Translator.tsx
@@ -39,102 +39,103 @@ const SelectLanguage = ({
   );
 };
 
-export const Translator = () => {
-  const tr = useTranslator();
+ const Translator = () => {
+   const tr = useTranslator();
 
-  const { input, handleInputChange, handleSubmit, completion, isLoading } =
-    useCompletion({
-      api: "/api/translator",
-      body: {
-        inputLanguage: tr.input.label,
-        outputLanguage: tr.output.label,
-      },
-    });
-  const { copied, copy } = useClipboard({ timeout: 2000 });
-  const matches = useMediaQuery("(max-width: 500px)");
-  return (
-    <form className=" w-full space-y-4" onSubmit={handleSubmit}>
-      <div className="mx-auto flex w-full flex-col rounded-xl bg-slate-800 px-2 py-4 shadow-lg ">
-        {/* // DIRECT WRAPPER */}
-        <div className="mb-2 flex flex-wrap border-0 border-b border-solid border-slate-500 md:flex-nowrap ">
-          {/* // INPUT language pane */}
-          <div className="w-full gap-1 flex-col-start">
-            <SelectLanguage
-              value={tr.input.value}
-              setValue={(value: string) => {
-                tr.setLanguage("input", {
-                  value,
-                  label: languages.find((obj) => obj.value == value)
-                    ?.label as string,
-                });
-              }}
-            />
+   const { input, handleInputChange, handleSubmit, completion, isLoading } =
+     useCompletion({
+       api: "/api/translator",
+       body: {
+         inputLanguage: tr.input.label,
+         outputLanguage: tr.output.label,
+       },
+     });
+   const { copied, copy } = useClipboard({ timeout: 2000 });
+   const matches = useMediaQuery("(max-width: 500px)");
+   return (
+     <form className=" w-full space-y-4" onSubmit={handleSubmit}>
+       <div className="mx-auto flex w-full flex-col rounded-xl bg-slate-800 px-2 py-4 shadow-lg ">
+         {/* // DIRECT WRAPPER */}
+         <div className="mb-2 flex flex-wrap border-0 border-b border-solid border-slate-500 md:flex-nowrap ">
+           {/* // INPUT language pane */}
+           <div className="w-full gap-1 flex-col-start">
+             <SelectLanguage
+               value={tr.input.value}
+               setValue={(value: string) => {
+                 tr.setLanguage("input", {
+                   value,
+                   label: languages.find((obj) => obj.value == value)
+                     ?.label as string,
+                 });
+               }}
+             />
 
-            <div className="w-full border-0 border-t border-solid border-slate-500 pl-1 pt-2">
-              <Textarea
-                value={input}
-                onChange={handleInputChange}
-                minRows={matches ? 6 : 14}
-                maxRows={matches ? 8 : 14}
-                autosize
-                w="100%"
-                placeholder="Enter text to translate"
-                classNames={{
-                  input:
-                    "!bg-transparent !border-none !p-0 !font-medium !text-base",
-                }}
-              />
-            </div>
-          </div>
+             <div className="w-full border-0 border-t border-solid border-slate-500 pl-1 pt-2">
+               <Textarea
+                 value={input}
+                 onChange={handleInputChange}
+                 minRows={matches ? 6 : 14}
+                 maxRows={matches ? 8 : 14}
+                 autosize
+                 w="100%"
+                 placeholder="Enter text to translate"
+                 classNames={{
+                   input:
+                     "!bg-transparent !border-none !p-0 !font-medium !text-base",
+                 }}
+               />
+             </div>
+           </div>
 
-          {/* // OUTPUT language pane */}
-          <div className="h-full w-full gap-1 flex-col-start">
-            <div className="w-full flex-row-between">
-              <ActionIcon onClick={tr.swapLanguages} size="lg" radius="md">
-                <TbSwitchHorizontal className="" size="17" />
-              </ActionIcon>
-              <SelectLanguage
-                value={tr.output.value}
-                setValue={(value: string) => {
-                  tr.setLanguage("output", {
-                    value,
-                    label: languages.find((obj) => obj.value == value)
-                      ?.label as string,
-                  });
-                }}
-              />
-            </div>
-            <div className="min-h-[5rem] w-full border-0 border-t border-solid border-slate-500 p-2 font-semibold text-primary-300 ">
-              {completion}
-            </div>
-          </div>
-        </div>
-        {completion && (
-          <div className="w-full flex-row-end">
-            <ActionIcon
-              variant="outline"
-              radius="md"
-              size="lg"
-              onClick={() => {
-                copy(completion);
-              }}
-            >
-              {copied ? <GiCheckMark /> : <MdContentCopy />}
-            </ActionIcon>
-          </div>
-        )}
-      </div>
-      <Button
-        type="submit"
-        size="lg"
-        mx="auto"
-        radius="xl"
-        w="100%"
-        loading={isLoading}
-        disabled={!input}
-      >
-        {isLoading ? "Translating..." : "Translate"}
-      </Button>
-    </form>
-  );
-};
+           {/* // OUTPUT language pane */}
+           <div className="h-full w-full gap-1 flex-col-start">
+             <div className="w-full flex-row-between">
+               <ActionIcon onClick={tr.swapLanguages} size="lg" radius="md">
+                 <TbSwitchHorizontal className="" size="17" />
+               </ActionIcon>
+               <SelectLanguage
+                 value={tr.output.value}
+                 setValue={(value: string) => {
+                   tr.setLanguage("output", {
+                     value,
+                     label: languages.find((obj) => obj.value == value)
+                       ?.label as string,
+                   });
+                 }}
+               />
+             </div>
+             <div className="min-h-[5rem] w-full border-0 border-t border-solid border-slate-500 p-2 font-semibold text-primary-300 ">
+               {completion}
+             </div>
+           </div>
+         </div>
+         {completion && (
+           <div className="w-full flex-row-end">
+             <ActionIcon
+               variant="outline"
+               radius="md"
+               size="lg"
+               onClick={() => {
+                 copy(completion);
+               }}
+             >
+               {copied ? <GiCheckMark /> : <MdContentCopy />}
+             </ActionIcon>
+           </div>
+         )}
+       </div>
+       <Button
+         type="submit"
+         size="lg"
+         mx="auto"
+         radius="xl"
+         w="100%"
+         loading={isLoading}
+         disabled={!input}
+       >
+         {isLoading ? "Translating..." : "Translate"}
+       </Button>
+     </form>
+   );
+ };
+ export default Translator;
