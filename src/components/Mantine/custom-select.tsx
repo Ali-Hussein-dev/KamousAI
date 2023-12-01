41fc72b5bdd6b3a80ecc44c7a5de7b520e5f7a2c
Ali <ali.hussein.pre@gmail.com>
2023-12-01 08:23:39 +0100

    fix: keep text size 16px on mobile

--- src/components/Mantine/custom-select.tsx
+++ src/components/Mantine/custom-select.tsx
@@ -8,7 +8,7 @@ export const CustomSelect = (props: SelectProps) => {
       classNames={{
         dropdown: "!bg-slate-700 !rounded-lg !border-[1px] !border-slate-600",
         option: "hover:!bg-slate-800 !rounded",
-        input: "!bg-slate-700 !border-[1px] !border-slate-600",
+        input: "!bg-slate-700 !border-[1px] !border-slate-600 text-base",
       }}
       {...props}
     />
