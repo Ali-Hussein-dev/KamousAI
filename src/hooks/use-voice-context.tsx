9514051ca785f90235b5eb22ce9a463f2d894c95
Ali <ali.hussein.pre@gmail.com>
2023-12-06 15:59:24 +0100

    fix: #46

---
+++ src/hooks/use-voice-context.tsx
@@ -0,0 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
+import * as React from "react";
+export const useVoiceContext = ({ text }: { text: string }) => {
+  const audioContext = React.useRef(new AudioContext());
+  const audioSource = React.useRef<AudioBuffer | null>(null);
+  const res = useQuery({
+    queryKey: ["audio", text],
+    queryFn: () =>
+      fetch("/api/text-to-speech", {
+        method: "POST",
+        body: JSON.stringify({ term: text }),
+      })
+        .then((d) => d.arrayBuffer())
+        .then((arrayBuffer) =>
+          audioContext.current?.decodeAudioData(arrayBuffer)
+        )
+        .then((decodedAudio) => {
+          audioSource.current = decodedAudio;
+          return decodedAudio;
+        }),
+    enabled: false,
+    staleTime: Infinity,
+  });
+  const { refetch, isFetching } = res;
+  const play = async () => {
+    await refetch();
+    const playsound = audioContext.current?.createBufferSource();
+    playsound.buffer = audioSource.current;
+    playsound.connect(audioContext.current.destination);
+    playsound.start(audioContext.current.currentTime);
+  };
+  return { play, isFetching };
+};
