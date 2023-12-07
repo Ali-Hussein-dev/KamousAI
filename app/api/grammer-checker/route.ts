d52e7ba9beca83eaaaaef1df7af476dec1b06583
Ali <ali.hussein.pre@gmail.com>
2023-12-07 14:43:29 +0100

    fix: #58

--- app/api/grammer-checker/route.ts
+++ app/api/grammer-checker/route.ts
@@ -13,8 +13,8 @@ export const runtime = "edge";
 const makeInstructions = (withExplanation: boolean) => `
   Before you start, follow these rules strictly:
   - Highlight the text with bolding the added text parts & strike through the deleted text parts
-  - Don't answer questions, just correct the grammar, spelling, and punctuation
-  ${withExplanation && "- Justify the correction briefly & clearly"}
+  - Don't answer questions, correct the grammar, spelling, and punctuation
+  ${withExplanation && "- Justify the correction briefly & clearly since I am learning the language!"}
 
   Correct the grammar and spelling mistakes of the following text
 `
@@ -27,9 +27,10 @@ export async function POST(req: Request) {
   // Extract the `messages` from the body of the request
   const json = await req.json();
   const { messages, withExplanation } = json;
+  console.log(makeInstructions(withExplanation))
   const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    // model: "gpt-3.5-turbo-1106",
+    // model: "gpt-3.5-turbo",
+    model: "gpt-3.5-turbo-1106",
     // model: "gpt-4-1106-preview",
     stream: true,
     messages: [
