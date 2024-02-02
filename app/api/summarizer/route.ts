241e2c5e12b09963f622a7d8931ce641df5e08fa
Ali <ali.hussein.pre@gmail.com>
2024-02-02 19:16:25 +0100

    feat: improve the summarizer

--- app/api/summarizer/route.ts
+++ app/api/summarizer/route.ts
@@ -5,6 +5,6 @@ export const runtime = "edge";
 
 export async function POST(req: Request) {
     const body = await req.json();
-    const { messages } = body;
-    return await createChatStream({ messages: [{ role: "system", content: CreatePrompt.summarize("paraghraph") }, ...messages] });
+    const { messages, mode } = body;
+    return await createChatStream({ messages: [{ role: "system", content: CreatePrompt.summarize(mode) }, ...messages] });
 }
--- src/utils/prompt-builder.ts
+++ src/utils/prompt-builder.ts
@@ -11,8 +11,8 @@ export class CreatePrompt {
   Correct the grammar and spelling mistakes of the following text
 `;
     }
-    static summarize(mode: "paraghraph" | "bulletsPoint" = "paraghraph") {
-        return `Summarize the following ${mode === "paraghraph" ? "text" : "bullet points"} into a paragraph`;
+    static summarize(mode: "paraghraph" | "bullet points" = "paraghraph") {
+        return `Summarize the following into ${mode} with a title (bold formated)`;
     }
     static reverseDictionary(suggestionsCount: 3 | 5 = 3) {
         return `I will provide you with the definition of a word, and your task is to suggest up to ${suggestionsCount} commonly-used synonyms. Make sure not to include the original word in your suggestions. The results should be presented in a table with three columns: the first column for the synonym, the second for its sentiment, and the third for the context in which the synonym is most frequently used.`;
