b3440902533dbaa5f938b5d5d52c4f1115239bf4
Ali <ali.hussein.pre@gmail.com>
2024-02-03 10:56:05 +0100

    feat: create a reusable tool container

---
+++ src/components/tool-container.tsx
@@ -0,0 +1,31 @@
+import { Title } from "@mantine/core";
+
+const titles = {
+  reverseDictionary: "Reverse Dictionary",
+  summarizer: "Summarizer",
+  grammarCorrector: "Grammar Corrector",
+  paraphraser: "Paraphraser",
+};
+
+//======================================
+export const ToolContainer = ({
+  children,
+  title,
+}: {
+  children: React.ReactNode;
+  title: keyof typeof titles;
+}) => {
+  return (
+    <section className="w-full rounded-lg bg-slate-800 px-3 py-6">
+      <Title
+        order={1}
+        mb="5px"
+        className="center text-lg uppercase"
+        c="#cbd5e1"
+      >
+        {titles[title]}
+      </Title>
+      {children}
+    </section>
+  );
+};
