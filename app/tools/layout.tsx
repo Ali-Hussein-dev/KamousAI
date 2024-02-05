3ebae607b99a2ae30fa344c499cfbe45879a191d
Ali <ali.hussein.pre@gmail.com>
2024-02-05 21:08:06 +0100

    refactor: remove multiple Mantine provider instances

--- app/tools/layout.tsx
+++ app/tools/layout.tsx
@@ -2,9 +2,8 @@
 import { toolsLinks, DynamicToolsBar } from "@/components";
 import { capitalizeFirstLetter, cn } from "@/utils/helpers";
 import { usePathname, useSelectedLayoutSegment } from "next/navigation";
-import { MantineProvider, AppShell, Burger } from "@mantine/core";
+import { AppShell, Burger } from "@mantine/core";
 import * as React from "react";
-import { theme } from "../../theme";
 import Link from "next/link";
 import { useDisclosure } from "@mantine/hooks";
 import Image from "next/image";
@@ -27,96 +26,92 @@ export default function ToolsLayout({
     return (
       <>
         <title>{title}</title>
-        <MantineProvider defaultColorScheme="dark" theme={theme}>
-          {children}
-        </MantineProvider>
+        {children}
       </>
     );
   return (
     <>
       <title>{title}</title>
-      <MantineProvider defaultColorScheme="dark" theme={theme}>
-        <AppShell
-          header={{ height: 55 }}
-          navbar={{
-            width: "240px",
-            breakpoint: "sm",
-            collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
-          }}
+      <AppShell
+        header={{ height: 55 }}
+        navbar={{
+          width: "240px",
+          breakpoint: "sm",
+          collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+        }}
+      >
+        <AppShell.Header
+          withBorder={false}
+          classNames={{ header: "bg-slate-700/80 backdrop-blur" }}
         >
-          <AppShell.Header
-            withBorder={false}
-            classNames={{ header: "bg-slate-700/80 backdrop-blur" }}
-          >
-            <div className="h-full px-4">
-              <div className="h-full gap-2  flex-row-start">
-                {/* <Burger
+          <div className="h-full px-4">
+            <div className="h-full gap-2  flex-row-start">
+              {/* <Burger
                   opened={mobileOpened}
                   onClick={toggleMobile}
                   hiddenFrom="sm"
                   size="sm"
                 /> */}
-                <Burger
-                  opened={desktopOpened}
-                  onClick={toggleDesktop}
-                  visibleFrom="sm"
-                  size="sm"
-                />
-                <div className="relative aspect-video h-10">
-                  <Link href="/">
-                    <Image src="/logo.svg" fill alt="logo" />
-                  </Link>
-                </div>
+              <Burger
+                opened={desktopOpened}
+                onClick={toggleDesktop}
+                visibleFrom="sm"
+                size="sm"
+              />
+              <div className="relative aspect-video h-10">
+                <Link href="/">
+                  <Image src="/logo.svg" fill alt="logo" />
+                </Link>
               </div>
             </div>
-          </AppShell.Header>
+          </div>
+        </AppShell.Header>
 
-          <AppShell.Navbar p="xs" withBorder={false}>
-            {/* <div className="relative h-10 w-32">
+        <AppShell.Navbar p="xs" withBorder={false}>
+          {/* <div className="relative h-10 w-32">
               <Image src="/logo.svg" fill alt="logo" />
             </div> */}
-            <div className="h-full space-y-2 border-0 border-r border-solid border-slate-500 pr-4 pt-1">
-              {toolsLinks.map((item) => (
-                <Link
-                  href={item.href}
-                  key={item.label}
-                  className="group block rounded px-2 py-2 text-slate-200 no-underline"
-                >
-                  <div className="gap-2 flex-row-start">
-                    <span
-                      className={cn(
-                        "center h-8 w-8 rounded-lg border border-solid border-slate-500 duration-300 group-hover:border-transparent group-hover:bg-primary-600",
-                        pathname == item.href &&
-                          "border-transparent bg-primary-600"
-                      )}
-                    >
-                      {item.icon}
-                    </span>
-                    <span className="font-medium">{item.label}</span>
-                  </div>
-                </Link>
-              ))}
-            </div>
-          </AppShell.Navbar>
-
-          <AppShell.Main py="lg" px="0" pb="0">
-            <div className="flex">
-              <div
-                className={cn(
-                  "hidden h-full md:block",
-                  !desktopOpened ? "hidden" : "w-[240px]"
-                )}
-              ></div>
-              <div className="grow px-4 py-1">
-                <div className="mx-auto max-w-4xl pb-28 pt-10 sm:pb-4">
-                  {children}
+          <div className="h-full space-y-2 border-0 border-r border-solid border-slate-500 pr-4 pt-1">
+            {toolsLinks.map((item) => (
+              <Link
+                href={item.href}
+                key={item.label}
+                className="group block rounded px-2 py-2 text-slate-200 no-underline"
+              >
+                <div className="gap-2 flex-row-start">
+                  <span
+                    className={cn(
+                      "center h-8 w-8 rounded-lg border border-solid border-slate-500 duration-300 group-hover:border-transparent group-hover:bg-primary-600",
+                      pathname == item.href &&
+                        "border-transparent bg-primary-600"
+                    )}
+                  >
+                    {item.icon}
+                  </span>
+                  <span className="font-medium">{item.label}</span>
                 </div>
+              </Link>
+            ))}
+          </div>
+        </AppShell.Navbar>
+
+        <AppShell.Main py="lg" px="0" pb="0">
+          <div className="flex">
+            <div
+              className={cn(
+                "hidden h-full md:block",
+                !desktopOpened ? "hidden" : "w-[240px]"
+              )}
+            ></div>
+            <div className="grow px-4 py-1">
+              <div className="mx-auto max-w-4xl pb-28 pt-10 sm:pb-4">
+                {children}
               </div>
             </div>
-            <DynamicToolsBar />
-          </AppShell.Main>
-        </AppShell>
-      </MantineProvider>
+          </div>
+          <DynamicToolsBar />
+        </AppShell.Main>
+      </AppShell>
     </>
   );
 }
