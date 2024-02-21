00b1581b5588fd9059666104dd2b3c68cfa90ebe
Ali <ali.hussein.pre@gmail.com>
2024-02-21 12:48:19 +0100

    feat: leverage VERCEL_BRANCH_URL for auth

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,31 +1,33 @@
-import { createClient } from '@/utils/supabase/server'
-import { NextResponse } from 'next/server'
-import { cookies } from 'next/headers'
+import { createClient } from "@/utils/supabase/server";
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 const getURL = () => {
     let url =
-        process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-        process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-        'http://localhost:3000/'
+        process.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+        process.env?.NEXT_PUBLIC_VERCEL_URL ??
+        process.env?.NEXT_PUBLIC_VERCEL_BRANCH_URL ??
+        // Automatically set by Vercel.
+        "http://localhost:3000/";
     // Make sure to include `https://` when not localhost.
-    url = url.includes('http') ? url : `https://${url}`
+    url = url.includes("http") ? url : `https://${url}`;
     // Make sure to include a trailing `/`.
-    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
-    return url
-}
+    url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+    return url;
+};
 
 export async function GET(request: Request) {
     // The `/auth/callback` route is required for the server-side auth flow implemented
     // by the Auth Helpers package. It exchanges an auth code for the user's session.
     // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
-    const requestUrl = new URL(request.url)
-    const code = requestUrl.searchParams.get('code')
+    const requestUrl = new URL(request.url);
+    const code = requestUrl.searchParams.get("code");
 
     if (code) {
-        const cookieStore = cookies()
-        const supabase = createClient(cookieStore)
-        await supabase.auth.exchangeCodeForSession(code)
+        const cookieStore = cookies();
+        const supabase = createClient(cookieStore);
+        await supabase.auth.exchangeCodeForSession(code);
     }
     // URL to redirect to after sign in process completes
-    return NextResponse.redirect(getURL() + "profile")
+    return NextResponse.redirect(getURL() + "profile");
 }
