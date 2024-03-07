7a9c69d6de204c73afb90e733e03e25a8e600a74
Ali <ali.hussein.pre@gmail.com>
2024-03-07 17:47:04 +0100

    refactor: remove class from global style

--- src/components/homepage/app-layout.tsx
+++ src/components/homepage/app-layout.tsx
@@ -6,7 +6,14 @@ import * as React from "react";
 
 export const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg">
+    <div
+      style={{
+        width: "100%",
+        backgroundRepeat: "repeat",
+        backgroundSize: "250px 250px",
+        backgroundImage: "url(/grid.svg)",
+      }}
+    >
       <AppShell
         classNames={{
           root: "from-slate-900 via-slate-900/80 to-slate-900 bg-gradient-to-r relative",
