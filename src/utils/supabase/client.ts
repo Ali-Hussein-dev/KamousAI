e2990d4001db5043e05e58136b76ef9125464128
Ali <ali.hussein.pre@gmail.com>
2024-03-09 00:04:31 +0100

    feat: type supabase utils

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -2,7 +2,7 @@ import { env } from '@/env.mjs'
 import { createBrowserClient } from '@supabase/ssr'
 
 export const createClient = () =>
-    createBrowserClient(
+    createBrowserClient<DB>(
         env.NEXT_PUBLIC_SUPABASE_URL,
         env.NEXT_PUBLIC_SUPABASE_ANON_KEY
     )
