138d96a8a0c999b0f2099098d30edff51fbea14f
Ali <ali.hussein.pre@gmail.com>
2024-03-19 13:41:48 +0100

    refactor: run prettier

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,10 @@ const Home: NextPage = () => {
     <AppLayout>
       <div className="center mx-auto h-[90vh] w-full max-w-4xl pb-3 pt-5">
         <div className="absolute right-0 top-0 h-[24rem] w-full bg-gradient-to-b from-slate-900/70 to-transparent sm:h-[30rem] lg:h-[44rem]"></div>
-        <div className="animate-in mb-12 flex-col-center">
+        <div className="mb-12 flex-col-center animate-in">
           <h1 className="h1 text-balance">Language Learning Tools</h1>
-          <h2 className="text-pretty mt-0 text-center text-base text-slate-400 md:text-2xl">
-            Advance your language skills with AI
+          <h2 className="mt-0 text-pretty text-center text-base text-slate-400 md:text-2xl">
+            Uplevel your language skills with AI
           </h2>
           <Link href="/tools/dictionary">
             <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] shadow-lg focus:outline-none focus:ring-1 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
@@ -25,6 +25,7 @@ const Home: NextPage = () => {
           </Link>
         </div>
       </div>
+
       <div className="mx-auto w-full max-w-6xl space-y-12">
         <ToolsBentoTools />
         <EasyAccess />
