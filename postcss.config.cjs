1cbe0926df52cc6a3ffc87bc833d5b17e0856e11
Ali <ali.hussein.pre@gmail.com>
2024-03-20 23:02:13 +0100

    chore: use tailwind breakpoints

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -5,11 +5,11 @@ const config = {
     'postcss-preset-mantine': {},
     'postcss-simple-vars': {
       variables: {
-        'mantine-breakpoint-xs': '36em',
-        'mantine-breakpoint-sm': '48em',
-        'mantine-breakpoint-md': '62em',
-        'mantine-breakpoint-lg': '75em',
-        'mantine-breakpoint-xl': '88em',
+        'mantine-breakpoint-xs': '425px',
+        'mantine-breakpoint-sm': '640px',
+        'mantine-breakpoint-md': '768px',
+        'mantine-breakpoint-lg': '1024px',
+        'mantine-breakpoint-xl': '1280px',
       },
     },
   },
