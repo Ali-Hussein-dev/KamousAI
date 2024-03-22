50b212b9c01ca3dd01619588cfd742e41eacc1ce
Ali <ali.hussein.pre@gmail.com>
2024-03-22 15:14:46 +0100

    style: tweak UI

--- src/components/homepage/easy-access.tsx
+++ src/components/homepage/easy-access.tsx
@@ -1,4 +1,3 @@
-import { Text, Title } from "@mantine/core";
 import { FaWindows, FaAppStore, FaApple } from "react-icons/fa6";
 import { DiAndroid } from "react-icons/di";
 import Image from "next/image";
@@ -7,24 +6,21 @@ export const EasyAccess = () => {
   return (
     <div className="mx-auto rounded-xl pb-6 pt-10 md:pt-16">
       <div className="mx-auto max-w-fit ">
-        <Title order={2} ta="center">
+        <h2 className="mb-3 text-center text-2xl font-extrabold sm:text-3xl md:text-5xl">
           Easy Access
-        </Title>
-        <Text
-          ta="center"
-          className="text-balance max-w-xl pt-1 text-lg font-medium"
-        >
+        </h2>
+        <p className="mt-0 max-w-xl pt-1 text-center sm:text-lg">
           KamousAI is a progressive web app(PWA), simply install it on any
           device using the browser.
-        </Text>
+        </p>
       </div>
-      <div className="gap-8 pt-6 flex-row-center">
-        <DiAndroid size="64" />
-        <FaAppStore size="64" />
-        <FaWindows size="64" />
-        <FaApple size="64" />
+      <div className="gap-4 pt-6 flex-row-center">
+        <DiAndroid size="42" />
+        <FaAppStore size="42" />
+        <FaWindows size="42" />
+        <FaApple size="42" />
       </div>
-      <div className="pt-6 flex-col-center">
+      <div className="pt-3 flex-col-center">
         <a
           href="https://play.google.com/store/apps/details?id=io.noorai.kamous.twa"
           target="_blank"
@@ -33,8 +29,8 @@ export const EasyAccess = () => {
           <Image
             src="/google-play-badge.png"
             alt="google play badge"
-            width={260}
-            height={95}
+            width={240}
+            height={82}
           />
         </a>
       </div>
