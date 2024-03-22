503aa16bd099259a500858ad3af69bb0518c491c
Ali <ali.hussein.pre@gmail.com>
2024-03-22 14:59:11 +0100

    style: tweak the bg

--- src/components/homepage/open-source.tsx
+++ src/components/homepage/open-source.tsx
@@ -4,26 +4,28 @@ import { BsGithub } from "react-icons/bs";
 //======================================
 export const OpenSource = () => {
   return (
-    <div className="pt-20">
-      <div className="center bg-slate-100 px-2 py-10 text-slate-800 md:py-24">
-        <h3 className="mb-2 text-2xl font-extrabold sm:text-3xl md:text-5xl">
-          Proudly open-source
-        </h3>
-        <p className="mt-0 max-w-xl text-center sm:text-xl">
-          Our source code is available on GitHub; feel free to read, review, or
-          contribute to it in any way you like!
-        </p>
-        <Button
-          component="a"
-          className=""
-          bg="black"
-          size="lg"
-          radius="md"
-          href="https://github.com/Ali-Hussein-dev/KamousAI"
-          leftSection={<BsGithub />}
-        >
-          View in GitHub
-        </Button>
+    <div className=" pt-20">
+      <div className="rotate-[1.5deg] bg-gradient-to-r from-theme-accent to-theme-primary">
+        <div className="center -rotate-[1.5deg] bg-slate-200 px-2 py-10 text-slate-800 shadow md:py-24">
+          <h3 className="mb-2 text-2xl font-extrabold sm:text-3xl md:text-5xl">
+            Proudly open-source
+          </h3>
+          <p className="mt-0 max-w-xl text-center sm:text-xl">
+            Our source code is available on GitHub; feel free to read, review,
+            or contribute to it in any way you like!
+          </p>
+          <Button
+            component="a"
+            className=""
+            bg="black"
+            size="lg"
+            radius="md"
+            href="https://github.com/Ali-Hussein-dev/KamousAI"
+            leftSection={<BsGithub />}
+          >
+            View in GitHub
+          </Button>
+        </div>
       </div>
     </div>
   );
