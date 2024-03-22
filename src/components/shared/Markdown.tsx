67b273dae6a2e824c2206c12c9fc63ed7448218b
Ali <ali.hussein.pre@gmail.com>
2024-03-22 21:33:15 +0100

    feat: make response cards deletable

--- src/components/shared/Markdown.tsx
+++ src/components/shared/Markdown.tsx
@@ -1,11 +1,21 @@
+import { cn } from "@/utils/helpers";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
-export const Markdown = ({ children }: { children: string }) => {
+export const Markdown = ({
+  children,
+  tw,
+}: {
+  children: string;
+  tw?: string;
+}) => {
   return (
     <ReactMarkdown
       remarkPlugins={[[remarkGfm, { singleTilde: false }]]}
-      className="prose prose-slate mb-2 max-w-full tracking-wide prose-p:mt-2 prose-strong:font-extrabold prose-th:py-1 prose-th:uppercase prose-th:text-teal-400"
+      className={cn(
+        "prose prose-slate mb-2 max-w-full tracking-wide prose-p:mt-2 prose-strong:font-extrabold prose-th:py-1 prose-th:uppercase prose-th:text-teal-400",
+        tw
+      )}
     >
       {children}
     </ReactMarkdown>
