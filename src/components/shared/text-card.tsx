97539b0e5423e4d977d57b95a3f1837b660b6186
Ali <ali.hussein.pre@gmail.com>
2024-03-22 22:43:10 +0100

    style: use display instead of opacity

--- src/components/shared/text-card.tsx
+++ src/components/shared/text-card.tsx
@@ -13,11 +13,11 @@ export const TextCard = ({
   drop?: () => void;
 }) => {
   return (
-    <div className="group flex items-start justify-between gap-2 rounded bg-slate-700/50 px-3 pb-2 pt-4 text-slate-200">
+    <div className="group flex items-start justify-between gap-2 rounded bg-slate-700/50 px-3 pb-2 pt-4 text-slate-200 duration-1000">
       <Markdown tw="prose-p:my-0">{content}</Markdown>
       <div
         className={cn(
-          "gap-3 duration-300 flex-col-center group-hover:opacity-100 md:opacity-0"
+          "gap-3 duration-300 flex-col-center group-hover:flex md:hidden"
         )}
       >
         <CopyButton text={content} />
