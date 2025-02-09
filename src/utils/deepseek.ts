16744467f3011675a5d532a5152559ff6755a332
Ali <ali.hussein.pre@gmail.com>
2025-02-09 13:01:52 +0100

    fix: remove log & update ai package

--- src/utils/deepseek.ts
+++ src/utils/deepseek.ts
@@ -1,4 +1,4 @@
-import { type CoreMessage, smoothStream, streamText } from "ai";
+import { type CoreMessage, streamText } from "ai";
 import { deepseek } from "@ai-sdk/deepseek";
 
 export async function createChatStreamDeepSeek(configs: {
@@ -6,16 +6,14 @@ export async function createChatStreamDeepSeek(configs: {
     system?: string,
     temperature?: number,
 }) {
-    console.log({apiKey: process.env.DEEPSEEK_API_KEY})
     const res =  streamText({
         model: deepseek("deepseek-chat"),
         ...configs,
         messages: configs.messages,
-
-        experimental_transform: smoothStream({
-            // delayInMs: 12, // optional: defaults to 10ms
-            delayInMs: 12
-        }),
+        // experimental_transform: smoothStream({
+        //     // delayInMs: 12, // optional: defaults to 10ms
+        //     delayInMs: 12
+        // }),
     });
     return res.toDataStreamResponse();
 }
